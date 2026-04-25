63a1b4f34a28022bd3184451c5bf38fb5f8e7572
Nakul <nakulsukhwani.in@gmail.com>
2026-04-25 17:20:50 +0530

    maxsum_subarray_optimization_1

--- code.cpp
+++ code.cpp
@@ -5,15 +5,12 @@ using namespace std;
 void max_subarray_sum(int *arr, int n ){
     int maxsum = INT_MIN;
     for(int st = 0 ; st<n; st++){
+        int currsum = 0;
         for(int end = st ; end<n ; end++){
-            int currsum = 0;
-            for(int i = st ; i<=end ; i++){
-                currsum += arr[i];
-            }
-            cout<<currsum<<",";
+            currsum += arr[end];
             maxsum = max(maxsum ,currsum);  
         }
-        cout<<endl;
+        
     }
     cout<<"maximum subarray sum = "<<maxsum<<endl;
 }
@@ -21,5 +18,5 @@ int main(){
     int arr[] = {2, -3, 6, -5, 4, 2};
     int n = sizeof(arr)/sizeof(int);
     max_subarray_sum(arr, n);
-    return 0;
-}
+    return 0;   
+} 
