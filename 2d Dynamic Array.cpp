66b754370ea888c1f232d3222dcb481ffa186501
Nakul <nakulsukhwani.in@gmail.com>
2026-06-28 22:06:18 +0530

    Pair_Sum

--- 2d Dynamic Array.cpp
+++ 2d Dynamic Array.cpp
@@ -8,7 +8,6 @@ int main (){
     cin>>rows;
     cout<<"enter Columns : ";
     cin>>cols;
- 
     int* *matrix= new int*[rows];
     for(int i = 0 ;  i<rows ;i++){
         matrix[i]= new int [cols];
