e8ab45cac1ea49b1a4cb0bb5621035428fa64a2f
Nakul <nakulsukhwani.in@gmail.com>
2026-07-03 01:48:32 +0530

    multilevel  inheritance

--- 04_Object_Oriented_Programming/Multilevel inheritance.cpp
+++ 04_Object_Oriented_Programming/Multilevel inheritance.cpp
@@ -13,12 +13,19 @@ class Animal{
     }
 
 };
-class Fish : public Animal{ //this shows Here the single inheritance happening over here! 
+class Fish : public Animal{ //this shows Here the inheritance happening over here! 
     public:
     int fins;
     void swim(){
         cout<<"swims---\n";
     }
+};
+class Whale : public Fish {// here is eg: of Multi level inheritance !
+    public: 
+    void Mammal(){
+        cout<<"Whale is a Mammal\n";
+    }
+
 };
 
 int main(){
@@ -30,5 +37,13 @@ f1.breath();
 f1.eat();
 f1.color = "Blue";
 cout<<f1.color<<endl;
+
+cout<<"---multilevel inheritance---\n";
+
+Whale w1;
+w1.breath();
+w1.eat();
+w1.swim();
+w1.Mammal();
     return 0; 
 }
